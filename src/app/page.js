c3349763369e8615ec6cc482a9e4e78a550e4b8f
harrionparrix <varunpat44@gmail.com>
2023-11-04 16:05:10 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import ResponsiveAppBar from "../components/appBar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import styles from "./page.module.css";
-import FAQSection from "@/components/faqSection";
+import FAQSection from "src/components/faqSection.js";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
