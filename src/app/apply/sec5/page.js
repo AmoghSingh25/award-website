b7c3fe0d9c9663e4e0f6da44fda7477e224c20fc
Amogh Singh <amoghanuj@gmail.com>
2023-12-07 14:59:51 +0530

    Loading true

--- src/app/apply/sec5/page.js
+++ src/app/apply/sec5/page.js
@@ -124,7 +124,7 @@ export default function Page() {
     //       data.other_documents,
     //       setOtherDocs
     //     );
-    //     setLoading(false);
+    setLoading(false);
     //   });
   }, []);
 
